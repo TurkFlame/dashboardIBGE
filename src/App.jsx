9ee4cef32db939f8e6f4cece17e0145475870e86
TurkFlame <luizfelipemarcondesdelima@gmail.com>
2024-05-01 14:52:33 -0300

    First Commit

--- src/App.jsx
+++ src/App.jsx
@@ -13,13 +13,10 @@ function App() {
     api.getPIBPerCapita().then(data => setPibPerCapita(data));
   }, []);
 
+  {/* Precisa de um lugar para o usuário poder colocar para filtrar os paises e a filtragem por data*/}
   return (
     <div id='Infos'>
-      <h1>Dashboard</h1>
-      <p>Total PIB:</p>
-      <pre>{JSON.stringify(totalPib, null, 2)}</pre>
-      <p>PIB per Capita:</p>
-      <pre>{JSON.stringify(pibPerCapita, null, 2)}</pre>
+      {/* Aqui vai ir os dados para construção do dashboard */}
     </div>
   );
 }

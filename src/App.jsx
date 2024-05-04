273ff2453e8feffe189841d696641ca05ae80e7a
Braio <mernickbryan@gmail.com>
2024-05-03 22:47:12 -0300

    feat: add theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Api from './model/api.jsx';
 import Filter from './model/filter.jsx';
+import { ThemeProvider } from "@/components/theme-provider"
 
 function App() {
   console.log("oi")
@@ -13,11 +14,13 @@ function App() {
     api.getPIBPerCapita().then(data => setPibPerCapita(data));
   }, []);
 
-  {/* Precisa de um lugar para o usuário poder colocar para filtrar o tipo de pesquisa que ele quer, exemplo: pibPerCapita, pibTotal, filtrar os paises e filtragem por data*/}
+  {/* Precisa de um lugar para o usuário poder colocar para filtrar o tipo de pesquisa que ele quer, exemplo: pibPerCapita, pibTotal, filtrar os paises e filtragem por data*/ }
   return (
-    <div id='Infos'>
-      {/* Aqui vai ir os dados para construção do dashboard */}
-    </div>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div id='Infos'>
+        {/* Aqui vai ir os dados para construção do dashboard */}
+      </div>
+    </ThemeProvider>
   );
 }
 

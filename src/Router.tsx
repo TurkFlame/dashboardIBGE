7e26014b806985f218ad8a53c38df448aa00c4a8
TurkFlame <luizfelipemarcondesdelima@gmail.com>
2024-05-07 23:31:59 -0300

    + Api nomes, + Api mesh + Api Economia. + Retorno do mesh no endpoint /getNameRanking

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,17 +1,18 @@
 import { Routes, Route } from 'react-router-dom';
-import Entrance from './pages/entrance';
+// import Entrance from './pages/entrance';
 import Dashboard from './pages/dashboard';
-import Dashboards from './pages/dashboards';
-import LoginLogout from './pages/loginAndLogout';
+// import Dashboards from './pages/dashboards';
+// import LoginLogout from './pages/loginAndLogout';
 import React from "react";
 
 export default function Router() {
     return (
         <Routes>
-            <Route path="/" element={<Entrance/>} />
+            {/* <Route path="/" element={<Entrance/>} />
             <Route path="/loginLogout" element={<LoginLogout/>} />
-            <Route path="/dashboards/" element={<Dashboards />} /> {/* Tela para centralizar as dashboards*/} 
+            <Route path="/dashboards/" element={<Dashboards />} /> */}
             <Route path="/dashboard" element={<Dashboard />} /> {/* Tela para cada dashboard*/} 
+            <Route path="/getNameRanking" element={<Dashboard />} /> {/* Tela para cada dashboard*/} 
         </Routes>
     );
 }
--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,7 +1,24 @@
-import {React, UseEffect, UseState} from 'react';
+import React, { useEffect, useState } from "react";
+import Api from "../services/api";
 
-function DashBoard() { 
-    useEffect({
-        
-    },[])
+export default function DashBoard() {
+  const [svg, setSvg] = useState(null);
+
+  useEffect(() => {
+    const apiInstance = new Api();
+    apiInstance.getMeshByCountryId('BR')
+  .then((dataSvg) => {
+        // Certifique-se de que dataSvg é uma string SVG válida
+        setSvg(dataSvg);
+      })
+  .catch((error) => {
+        console.error("Erro = ", error);
+      });
+  }, []);
+
+  return (
+    <>
+      {svg && <div dangerouslySetInnerHTML={{ __html: svg }} />}
+    </>
+  )
 }

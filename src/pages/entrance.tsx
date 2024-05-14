1588c1f23eca3bd1686646380c4a09eefb1bb201
Braio <mernickbryan@gmail.com>
2024-05-13 21:33:44 -0300

    feat: adição de foto no carrousel

--- src/pages/entrance.tsx
+++ src/pages/entrance.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import IbgeImgNome from '@/assets/images/ibge-img-nome.png';
 
 export default function Entrance() {
   const settings = {
@@ -10,15 +11,17 @@ export default function Entrance() {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 5000
+    autoplay: false,
+    autoplaySpeed: 5000,
+    centerMode: true,
+    centerPadding: '0px',
   };
 
   return (
       <div className='carrousel'>
-        <Slider {...settings} style={{ width: '600px', height: '500px' }}>
-          <div>
-            <img src="image1.jpg" alt="Imagem 1" />
+        <Slider className='slick-slider' {...settings}>
+          <div className='slide'>
+            <img src={IbgeImgNome} alt="Imagem 1" width={300} height={200}/>
           </div>
           <div>
             <img src="image2.jpg" alt="Imagem 2" />

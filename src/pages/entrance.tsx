1ea4a73ded3eb9b04f54cacc3e6684de1265e256
Braio <mernickbryan@gmail.com>
2024-05-22 22:01:12 -0300

    feat: tentativa carrousel

--- src/pages/entrance.tsx
+++ src/pages/entrance.tsx
@@ -20,21 +20,66 @@ export default function Entrance() {
   };
 
   return (
-    <div className='carrousel'>
-      <Navbar />
-      <Slider className='slick-slider' {...settings}>
-        <div className='slide'>
-          <Link to="/page-name" className='link-invisivel'>
-            <img src={IbgeImgNome} alt="Imagem 1" width={300} height={200} />
-          </Link>
+    <div className='row'>
+      <div className="container-carrousel">
+        <div className="card">
+          <h3 className="title">Nomes</h3>
+          <div className="bar">
+            <div className="emptybar"></div>
+            <div className="filledbar"></div>
+          </div>
+          <div className="circle">
+
+
+            <Link to="/page-name" className='link-invisivel'>
+              {/* <img src={IbgeImgNome} alt="Imagem 1" width={300} height={200} /> */}
+              <svg version="1.1" xmlns="http://www.w3.org/2000/svg">
+                <circle className="stroke" cx="60" cy="60" r="50" />
+              </svg>
+            </Link>
+          </div>
         </div>
-        <div>
-          <img src="image2.jpg" alt="Imagem 2" />
+        <div className="card">
+          <h3 className="title">Pesquisas</h3>
+          <div className="bar">
+            <div className="emptybar"></div>
+            <div className="filledbar"></div>
+          </div>
+          <div className="circle">
+            <Link to="/page-search" className='link-invisivel'>
+              {/* <img src={IbgeImgNome} alt="Imagem 1" width={300} height={200} /> */}
+              <svg version="1.1" xmlns="http://www.w3.org/2000/svg">
+                <circle className="stroke" cx="60" cy="60" r="50" />
+              </svg>
+            </Link>
+          </div>
         </div>
-        <div>
-          <img src="image3.jpg" alt="Imagem 3" />
+        <div className="card">
+          <h3 className="title">Card 3</h3>
+          <div className="bar">
+            <div className="emptybar"></div>
+            <div className="filledbar"></div>
+          </div>
+          <div className="circle">
+            <svg version="1.1" xmlns="http://www.w3.org/2000/svg">
+              <circle className="stroke" cx="60" cy="60" r="50" />
+            </svg>
+          </div>
         </div>
-      </Slider>
+        <div className="card">
+          <h3 className="title">Card 4</h3>
+          <div className="bar">
+            <div className="emptybar"></div>
+            <div className="filledbar"></div>
+          </div>
+          <div className="circle">
+            <svg version="1.1" xmlns="http://www.w3.org/2000/svg">
+              <circle className="stroke" cx="60" cy="60" r="50" />
+            </svg>
+          </div>
+        </div>
+      </div>
     </div>
+
   );
 }

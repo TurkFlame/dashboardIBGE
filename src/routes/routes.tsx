90fadaf837853b2d8602be09c3b97cd0acdc16d2
TurkFlame <luizfelipemarcondesdelima@gmail.com>
2024-05-24 19:43:28 -0300

    feat: remoção do private

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -4,10 +4,7 @@ import Entrance from "../pages/entrance";
 import Economy from "../pages/economy";
 import PageName from "../pages/pageName";
 import PageSearch from "../pages/pageSearch";
-import SignIn from "../pages/signIn";
-import SignUp from "../pages/signUp";
 import Dashboards from "../pages/dashboards";
-import Private from "./private";
 
 export default function Router() {
   return (
@@ -15,9 +12,6 @@ export default function Router() {
       <Route path="/" element={<Entrance />} />
       <Route path="/page-name" element={<PageName />} />
       <Route path="/page-search" element={<PageSearch />} />
-      <Route path="/signin" element={<SignIn />} />{" "}
-      {/* Tela para ir para o login */}
-      <Route path="/signup" element={<SignUp />} />{" "}
       {/* Tela para ir para o cadastro */}
       <Route path="/dashboards" element={<Dashboards />} /> {/**/}
       <Route path="/page-economy" element={<Economy />} />{" "}

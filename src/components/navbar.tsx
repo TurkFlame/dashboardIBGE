4e7628518d9d80a5fdc6baeaf5947e8ceb6d554c
Braio <mernickbryan@gmail.com>
2024-06-02 01:30:28 -0300

    feat: melhoria na navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,18 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserGroup, faInbox } from '@fortawesome/free-solid-svg-icons';
+
 const Navbar = () => {
   return (
     <nav className="my-navbar d-flex">
-      <div className="navbar-logo-container">
+      <div>
         <Link to='/' className="my-navbar-logo">
-          EASY INFO
+          Easy Info
         </Link>
       </div>
       <div className="navbar-links">
         <Link to="/sobre-nos" className="my-navbar-link">
+          <FontAwesomeIcon icon={faUserGroup} className="icon-navbar" />
           Sobre nós
         </Link>
         <Link to="/contato" className="my-navbar-link">
+          <FontAwesomeIcon icon={faInbox} className="icon-navbar" />
           Contato
         </Link>
       </div>

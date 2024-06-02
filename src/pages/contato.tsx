8f58966f54c1f78b26dc3d7594fd039b00dcac5a
Braio <mernickbryan@gmail.com>
2024-06-02 01:49:32 -0300

    feat: melhoria da tela de contato e sobreNos

--- src/pages/contato.tsx
+++ src/pages/contato.tsx
@@ -5,20 +5,21 @@ const Contato: React.FC = () => {
   return (
       <div className="main-content">
         <div className="contato-container">
-          <h1>Contato</h1>
+          <h3>Contato</h3>
+          <hr></hr>
           <p>
-            Se você tiver alguma dúvida ou quiser saber mais sobre nossos serviços, entre em contato conosco através do formulário abaixo. 
+            Se você tiver alguma dúvida ou alguma sugestão, entre em contato conosco através do formulário abaixo. 
             Estamos ansiosos para ouvir de você!
           </p>
           <form>
             <label htmlFor="nome">Nome:</label>
-            <input type="text" id="nome" name="nome" required />
+            <input type="text" id="nome" name="nome" placeholder='Informe seu nome' required />
             
             <label htmlFor="email">E-mail:</label>
-            <input type="email" id="email" name="email" required />
+            <input type="email" id="email" name="email" required placeholder='Informe seu E-mail de contato' />
             
             <label htmlFor="mensagem">Mensagem:</label>
-            <textarea id="mensagem" name="mensagem" rows={5} required></textarea>
+            <textarea id="mensagem" name="mensagem" placeholder='Informe sua mensagem'  required></textarea>
             
             <button type="submit">Enviar</button>
           </form>
--- src/pages/sobreNos.tsx
+++ src/pages/sobreNos.tsx
@@ -1,43 +1,64 @@
 import React from 'react';
 import '../index.css';
-  
-  const SobreNos: React.FC = () => {
-    return (
-        <div className="main-content">
-        <div className="sobre-nos-container">
-          <h1>Sobre Nós</h1>
-          <p>
-            Projeto feito pelos alunos do 5° Periodo de engenharia de software,
-            utilizando a API gratuita do IBGE e trazendo dados em formas de gráficos e
-            tabelas para demonstração dessas pesquisas.
-          </p>
-          <section>
-            <h2>Nossa História</h2>
-            <p>
-              Juntando os alunos aos poucos, foi-se criando uma equipe incrivel e que se completa em todas
-              as areas, fazendo possivel entregar esse projeto incrivel que estamos vendo.
-            </p>
-          </section>
-          <section>
-            <h2>Nossos Valores</h2>
-            <ul>
-              <li>Inovação</li>
-              <li>Qualidade</li>
-              <li>Sustentabilidade</li>
-              <li>Integridade</li>
-              <li>Responsabilidade Social</li>
-            </ul>
-          </section>
-          <section>
-            <h2>Conheça Nossa Equipe</h2>
-            <p>
-              Nossa equipe é composta por profissionais altamente qualificados e
-              apaixonados pelo que fazem. Juntos, trabalhamos para entregar o melhor
-              para nossos projetos.
-            </p>
-          </section>
-        </div>
+
+const SobreNos: React.FC = () => {
+  return (
+    <div className="main-content mt-5">
+      <div className="sobre-nos-container">
+        <h3>Sobre Nós</h3>
+        <hr className='mt-1' />
+        <p className='mb-3'>
+          Projeto feito pelos alunos do 5° Periodo de engenharia de software,
+          utilizando a API gratuita do IBGE e trazendo dados em formas de gráficos e
+          tabelas para demonstração dessas pesquisas.
+        </p>
       </div>
+
+      <div className="sobre-nos-container">
+        <section>
+          <h3>Nossa História</h3>
+          <hr className='mt-1' />
+          <p className='mb-3'>
+            Juntando os alunos aos poucos, foi-se criando uma equipe incrivel e que se completa em todas
+            as areas, fazendo possivel entregar esse projeto incrivel que estamos vendo.
+          </p>
+        </section>
+      </div>
+
+      <div className="sobre-nos-container">
+        <section>
+          <h3>Nossos Valores</h3>
+          <hr className='mt-1' />
+          <ul className='mb-3'>
+            <li>Inovação</li>
+            <li>Qualidade</li>
+            <li>Sustentabilidade</li>
+            <li>Integridade</li>
+            <li>Responsabilidade Social</li>
+          </ul>
+        </section>
+      </div>
+
+      <div className="sobre-nos-container">
+        <section>
+          <h3>Conheça Nossa Equipe</h3>
+          <hr className='mt-1' />
+          <p className='mb-3'>
+            Nossa equipe é composta por alunos do Centro universitário Campo Real e
+            apaixonados pelo que fazem. Ao todo trabalhamos em 5 para a entrega deste projeto para 
+            entregar o melhor para nossos projetos.
+          </p>
+
+          <ul className='mb-3'>
+            <li>Bryan Mernick</li>
+            <li>Gabriel Vinicius Amtunes</li>
+            <li>Luisão</li>
+            <li>Ives Eduardo de Lara kracoki</li>
+            <li>Weslley césar zampier</li>
+          </ul>
+        </section>
+      </div>
+    </div>
   );
 };
 

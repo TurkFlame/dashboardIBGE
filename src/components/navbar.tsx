e765c1939f3d601f173c62b4ebcf3289008d6c3e
TurkFlame <luizfelipemarcondesdelima@gmail.com>
2024-06-04 22:11:33 -0300

    style: navbar style

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,20 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUserGroup, faInbox } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserGroup, faInbox } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
   return (
     <nav className="my-navbar d-flex">
       <div>
-        <Link to='/' className="my-navbar-logo">
-          Easy Info
+        <Link to="/" className="my-navbar-logo">
+          <span className="rubik-mono-one-regular">EASYINFO</span>
         </Link>
       </div>
       <div className="navbar-links">
         <Link to="/sobre-nos" className="my-navbar-link">
           <FontAwesomeIcon icon={faUserGroup} className="icon-navbar" />
-          Sobre nós
+          <span>Sobre nós</span>
         </Link>
         <Link to="/contato" className="my-navbar-link">
           <FontAwesomeIcon icon={faInbox} className="icon-navbar" />
